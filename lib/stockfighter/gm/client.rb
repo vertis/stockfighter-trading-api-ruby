d96a3a118c41585adf2f9e8697a0ec59a6651d87
Luke Chadwick <me@vertis.io>
2015-12-13 17:36:56 +1100

    Add start_level call to the GameMaster api client

--- lib/stockfighter/gm/client.rb
+++ lib/stockfighter/gm/client.rb
@@ -1,6 +1,17 @@
 module Stockfighter
   module GM
     class Client
+      include HTTParty
+      
+      base_uri "https://api.stockfighter.io/gm"
+
+      def initialize(options={})
+        self.class.headers options['headers'] if options['headers']
+      end
+
+      def start_level(name)
+        self.class.post("/levels/#{name}").parsed_response
+      end
     end
   end
 end
--- spec/stockfighter/gm/client_spec.rb
+++ spec/stockfighter/gm/client_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe Stockfighter::GM::Client do
+  describe '#start_level' do
+    subject { Stockfighter::GM::Client.new({ 'headers' => { 'X-Starfighter-Authorization' => 'fakeKey' } })}
+    it 'should start the level' do
+      VCR.use_cassette("start_level") do
+        res = subject.start_level('first_steps')
+        expect(res['ok']).to eq(true)
+        expect(res.keys).to eq(%w(account instanceId instructions ok secondsPerTradingDay tickers venues))
+      end
+    end
+  end
 end
