26bf2425f37d5cafc3ad584ab3d8080032a57f03
Luke Chadwick <me@vertis.io>
2015-12-27 15:15:17 +1100

    Switch gm client to use faraday

--- lib/stockfighter/gm/client.rb
+++ lib/stockfighter/gm/client.rb
@@ -1,33 +1,40 @@
 module Stockfighter
   module GM
     class Client
-      # include HTTParty
-      #
-      # base_uri "https://api.stockfighter.io/gm"
-      #
-      # def initialize(options={})
-      #   self.class.headers options['headers'] if options['headers']
-      # end
-      #
-      # def start_level(name)
-      #   self.class.post("/levels/#{name}").parsed_response
-      # end
-      #
-      # def restart_level(instance_id)
-      #   self.class.post("/instances/#{instance_id}/restart").parsed_response
-      # end
-      #
-      # def stop_level(instance_id)
-      #   self.class.post("/instances/#{instance_id}/stop").parsed_response
-      # end
-      #
-      # def resume_level(instance_id)
-      #   self.class.post("/instances/#{instance_id}/resume").parsed_response
-      # end
-      #
-      # def level_status(instance_id)
-      #   self.class.get("/instances/#{instance_id}").parsed_response
-      # end
+      def initialize(options={})
+        @connection = Faraday.new 'https://api.stockfighter.io/gm' do |conn|
+          conn.request :json
+
+          if options['headers']
+            conn.headers.merge!(options['headers'])
+          end
+
+          #conn.response :xml,  :content_type => /\bxml$/
+          conn.response :json #, :content_type => /\bjson$/
+
+          conn.adapter :typhoeus
+        end
+      end
+
+      def start_level(name)
+        @connection.post("levels/#{name}").body
+      end
+
+      def restart_level(instance_id)
+        @connection.post("instances/#{instance_id}/restart").body
+      end
+
+      def stop_level(instance_id)
+        @connection.post("instances/#{instance_id}/stop").body
+      end
+
+      def resume_level(instance_id)
+        @connection.post("instances/#{instance_id}/resume").body
+      end
+
+      def level_status(instance_id)
+        @connection.get("instances/#{instance_id}").body
+      end
     end
   end
 end
